f65752a2f4bd3f1951aea329e6e969026549f0e2
JanMultmeier <Jan.Multmeier@googlemail.com>
2015-05-07 09:25:24 +0200

    Added Streiktage.csv and worked on Bahnstreik-Verletzte.R

--- Bahnstreik_Verletzte.R
+++ Bahnstreik_Verletzte.R
@@ -16,3 +16,16 @@ data$Date <- as.Date(data$Date,format="%d-%m-%Y")
 data$Jahr <- factor(format(data$Date,format="%Y"))
 data$Monat <- factor(format(data$Date,format="%m"))
 data$Tag <- factor(format(data$Date,format="%d"))
+
+#2. Daten zu den Streiktagen (Selbst zusammengetrage)
+
+Streiktage <- read.csv2(file="/Users/Jan/Documents/GitHub/Bahnstreik/Streiktage.csv",col.names="Datum",stringsAsFactors=FALSE)
+Streiktage$Datum <- as.Date(Streiktage$Datum, format="%d.%m.%y")
+
+data$Streiktag <- character(length=nrow(data))
+
+for (i in seq_along(data$Date)) {
+  if (data$Date[i] %in% Streiktage$Datum) {
+    data$Streiktag[i] <- "Streik"
+  } else {data$Streiktag[i] <- "kein Streik"}
+}
